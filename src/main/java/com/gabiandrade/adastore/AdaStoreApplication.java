191686db87d3757ee9f4380dfaccc4519b9d320b
Gabriela Andrade <gabrielav.sudo@gmail.com>
2024-10-25 21:38:18 -0300

    aula 25/10

---
+++ src/main/java/com/gabiandrade/adastore/AdaStoreApplication.java
@@ -0,0 +1,13 @@
+package com.gabiandrade.adastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdaStoreApplication.class, args);
+    }
+
+}
